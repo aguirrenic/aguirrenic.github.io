7b9dc11a89b90bee750b052195f5156cc11eb648
rapaxai <rapaxai@gmail.com>
2026-02-14 10:42:07 -0300

    Change in main
    Change in main00

--- js/main.js
+++ js/main.js
@@ -8,12 +8,19 @@ function setLanguage(lang) {
 function applyTranslations() {
     document.querySelectorAll("[data-i18n]").forEach(el => {
         const key = el.getAttribute("data-i18n");
-        if (translations[currentLang][key]) {
-            el.innerText = translations[currentLang][key];
+
+        if (
+            typeof translations !== "undefined" &&
+            translations[currentLang] &&
+            key in translations[currentLang]
+        ) {
+            el.textContent = translations[currentLang][key];
+        } else {
+            console.warn("Missing translation key:", key);
         }
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     applyTranslations();
 });
